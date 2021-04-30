512fbdf76042591d7c6d81bc36435dcfa6db0f68
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-04-30 14:49:16 +0500

    Add sqlite db

--- migrations/20210430073331_create-lessons-table.js
+++ migrations/20210430073331_create-lessons-table.js
@@ -5,8 +5,17 @@ exports.up = function(knex) {
       tbl.text('name', 128).notNullable();
       tbl.timestamps(true, true)
   })
+  .createTable('messages', tbl => {
+      tbl.increments();
+      tbl.string('sender').notNullable().index()
+    tbl.text('text').notNullable()
+    tbl.timestamps(true, true)
+
+    tbl.integer('lesson_id').unsigned().references('id').inTable('lessons').onDelete('CASCADE').onUpdate('CASCADE')
+
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('messages').dropTableIfExists('lessons')
 };
