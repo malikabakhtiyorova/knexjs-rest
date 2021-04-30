a06bf1d5bb2ace60e83329d5ad16dbfab8d5956b
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-04-30 12:14:44 +0500

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+require('dotenv').config()
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+     host: 'localhost',
+     port: 5432,
+     user: 'postgres',
+     database:'postgres',
+     password: process.env.DB_PASSWORD
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
